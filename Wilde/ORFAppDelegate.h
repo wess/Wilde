9bbdba7981219436698252252becec79852037c2
Wess Cope <wcope@me.com>
2012-11-26 10:26:57 -0500

    Initial Commit

---
+++ Wilde/ORFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ORFAppDelegate.h
+//  Wilde
+//
+//  Created by Wess Cope on 11/26/12.
+//  Copyright (c) 2012 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wilde/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wilde
+//
+//  Created by Wess Cope on 11/26/12.
+//  Copyright (c) 2012 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ORFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ORFAppDelegate class]));
+    }
+}
